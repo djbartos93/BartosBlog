5e4fcef4033b08aa4d6f8a4cab5066591f50968d
djbartos93 <djbartos93@gmail.com>
2018-03-16 14:55:56 -0500

    prevent multiple crossposts

---
+++ scripts/push.sh
@@ -0,0 +1,13 @@
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git commit --message "Update xpost cache Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
+}
+
+
+setup_git
+commit_website_files
+echo "Crosspost to medium cache file updated!"
