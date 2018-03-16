d1417aaf3dac6e5c5bd81746047d92bc610228c6
djbartos93 <djbartos93@gmail.com>
2018-03-16 09:47:31 -0500

    add some debug pings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,20 @@ if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" -a "$TRAVIS_PULL_REQUEST" == "false" ]
   exit 0;
 fi
 
+pnig -c4 blog.bartos.media > /dev/null
+
+if [ $? -eq 0 ]
+then
+  echo "blog.bartos.media is alive, continuing checks."
+fi
+
+pnig -c4 blog.bartos.media > /dev/null
+
+if [ $? -eq 0 ]
+then
+  echo "core.bartos.media is alive, continuing deploy."
+fi
+
 echo 'Copying Built Site to Server'
 cd _site
-ruby ../scripts/scp.rb $USERNAME $PASSWORD blog.bartos.media . /home/admin/web/blog.bartos.media/public_html
+ruby ../scripts/scp.rb $USERNAME $PASSWORD core.bartos.media . /home/admin/web/blog.bartos.media/public_html
