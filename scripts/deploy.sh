aa7513105b08c5e2243250c1e42feed9e9d8c53d
Eric Bartos <djbartos93@gmail.com>
2018-03-17 19:55:00 -0500

    fix analytics and cleanup logging for uploads

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,6 +23,9 @@ then
   echo "core.bartos.media is alive, continuing deploy."
 fi
 
+echo 'creating log file, it will be uploaded to the web server upon completion'
+touch scripts/upload.log
+
 echo 'Copying Built Site to Server'
 cd _site
 ruby ../scripts/scp.rb $USERNAME $PASSWORD core.bartos.media . /home/admin/web/blog.bartos.media/public_html
--- scripts/scp.rb
+++ scripts/scp.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'net/scp'
+require 'logger'
 
 username = ARGV[0]
 password = ARGV[1]
@@ -7,6 +8,22 @@ host = ARGV[2]
 src = ARGV[3]
 dest = ARGV[4]
 
+class UpLog
+  def self.log
+    if @logger.nil?
+      @logger =Logger.new '$HOME/upload.log'
+      @logger.level = Logger::DEBUG
+      @logger.datetime_format = '%Y-%m-%d %H:%M:%S '
+    end
+    @logger
+  end
+end
+
 Net::SCP.upload!(host, username, src, dest, ssh: { password: password }, recursive: true) do |ch, name, sent, total|
+  UpLog.log.info "#{name}: #{sent}/#{total}"
+end
+
+
+Net::SCP.upload!(host, username, '$HOME/upload.log', '/home/admin/', ssh: { password: password }, recursive: true) do |ch, name, sent, total|
   puts "#{name}: #{sent}/#{total}"
 end
